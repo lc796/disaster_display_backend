242c6db0bad882a766b16dc8371bdfd08d73f412
Luke Collishaw <lc796@exeter.ac.uk>
2023-03-07 19:27:05 +0000

    Change update time to be suitable for production environment

--- disaster_app/fetch_scheduler/fetch_updater.py
+++ disaster_app/fetch_scheduler/fetch_updater.py
@@ -1,10 +1,17 @@
+import datetime
+
 from apscheduler.schedulers.background import BackgroundScheduler
 from disaster_app.views import DisasterViewset
 
 
 def start():
+    HOURS_BETWEEN_JOBS = 24
+
     scheduler = BackgroundScheduler()
     disaster = DisasterViewset()
 
-    scheduler.add_job(disaster.save_disaster_data, "interval", seconds=240, id="fetcher 01", replace_existing=True)
+    # Schedule tasks to run with an interval, and to start the first job after 3 seconds
+    scheduler.add_job(disaster.save_disaster_data, "interval", hours=HOURS_BETWEEN_JOBS,
+                      next_run_time=(datetime.datetime.now() + datetime.timedelta(seconds=3)),
+                      id="fetcher 01", replace_existing=True)
     scheduler.start()
